df95c21eff8aade4aa2bab1e85daf0ab24709341
khussey45 <khussey45@gmail.com>
2023-03-21 13:29:58 -0400

    d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Kieren</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I'm a computer programming student  <br className='sm:block hidden' />
+            at georgian college
           </p>
         </div>
       </div>
