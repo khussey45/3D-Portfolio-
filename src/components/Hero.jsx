c46e7255876d8fbd6151b18ffa0e910b705ac32a
khussey45 <khussey45@gmail.com>
2023-03-23 21:49:15 -0400

    c

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#0063DB]' />
+          <div className='w-1 sm:h-80 h-40 blue2-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Kieren</span>
+            Hi, I'm <span className='text-[#0063DB]'>Kieren</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a computer programming student  <br className='sm:block hidden' />
