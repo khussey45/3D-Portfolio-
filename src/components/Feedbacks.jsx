6dbc288ec49544947eb85ff6048eaadac2f36aff
khussey45 <khussey45@gmail.com>
2023-03-27 13:45:54 -0400

    d

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -49,7 +49,7 @@ const Feedbacks = () => {
       <div
         className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
       >
-        <motion.div variants={textVariant()}>
+        {/* <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
@@ -57,7 +57,7 @@ const Feedbacks = () => {
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
